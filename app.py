76f31070b39351765087dc083833850e13d1039d
sahil.designs.mail <sahil.designs.mail@gmail.com>
2024-12-15 13:36:22 +0530

    final update

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import google.generativeai as genai
-from flask import Flask,request,jsonify,render_template 
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -10,20 +10,18 @@ genai.configure(api_key="AIzaSyCvKTX7Ty7CXbAXnV9IqaH7eRmlfmIBen8")
 def index():
     return render_template('index.html')
 
-@app.route('/study',methods=["POST"])
+@app.route('/study', methods=["POST"])
 def myAI():
     user_query = request.json.get("query")
     if not user_query:
-        return jsonify({"error":"Query is Reqd."}),400
-    
+        return jsonify({"error": "Query is required."}), 400
+
     try:
-        
         model = genai.GenerativeModel("gemini-1.5-flash")
         response = model.generate_content(user_query)
-        
-        return jsonify({"response":response.text}),200
+        return jsonify({"response": response.text}), 200
     except Exception as e:
-        return jsonify({"error":str(e)}),500
-    
+        return jsonify({"error": str(e)}), 500
+
 if __name__ == "__main__":
     app.run(debug=True)
